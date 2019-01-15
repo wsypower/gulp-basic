19781fc6b8685005ff557d3bf7493a5b387b9674
weiyafei <2553241022@qq.com>
2019-01-15 14:17:45 +0800

    配置gulp基础文件

---
+++ dist/js/demo.js
@@ -0,0 +1,56 @@
+
+/**
+* 定义常量或变量
+* @var {object}
+* @desc
+* @property {string} a a 属性 a
+* @property {string} b b 属性 b
+*/
+
+var foo = {
+    a :'a',
+    b :'b'
+};
+
+/**
+* 定义公共函数
+* @method
+* @param {Type}  data  目标对象
+* @returns {Type} 匹配对象
+* @desc   根据目标对象获取匹配对象
+*/
+
+function matchedNumber(data){
+return '返回对象'
+}
+
+/**
+* 定义DOM操作或事件绑定
+* @method
+* @param {Type}  obj  目标对象
+* @param {Type}  type  绑定事件类型
+* @param {Type}  type  事件执行回调函数
+* @desc   绑定事件
+*/
+
+function addEvent(obj,type,handle) {
+
+    try{// Chrome、FireFox、Opera、Safari、IE9.0及其以上版本
+
+        obj.addEventListener(type,handle,false);
+
+    }catch(e){
+
+        try{// IE8.0及其以下版本
+
+        obj.attachEvent('on'+ type,handle);
+
+        }catch(e){// 早期浏览器
+
+            obj['on'+ type]= handle;
+
+        }
+
+    }
+
+}
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp = require('gulp'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  cssmin = require('gulp-clean-css'),
+  imagemin = require('gulp-imagemin'),
+  livereload = require('gulp-livereload'),
+  babel = require('gulp-babel'),
+  concat = require('gulp-concat'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  sourcemaps  = require('gulp-sourcemaps'),
+  pump = require('pump'),
+  rename = require('gulp-rename'),
+  plumber = require('gulp-plumber');
+var $ = require('gulp-load-plugins')();
+
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+//创建静态服务器并监听less文件和html文件的修改
+gulp.task('serve', ['cssFix', 'js'], function() {
+  browserSync.init({
+    server: "./dist/"
+  });
+  gulp.watch("src/scss/*.scss", ['cssFix']);
+  gulp.watch("src/js/*.js", ['js']);
+  // gulp.watch("dist/*.html").on('change', reload);
+  gulp.watch("src/js/*.js").on('change', reload);
+});
+
+//编译scss 自动增加后缀 css压缩
+
+gulp.task('cssFix', function() {
+  gulp.src('src/scss/*.scss')
+    .pipe(
+      sass()
+    )
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'Android >= 4.0'],
+      cascade: true, //是否美化属性值 默认：true 像这样：
+      remove: false //是否去掉不必要的前缀 默认：true
+    }))
+    // .pipe(cssmin({
+    //   keepSpecialComments: '*'
+    // }))
+    // .pipe(rename({
+    //   suffix: '.min'
+    // }))
+    // .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('js', function(cb) {
+  pump([
+      gulp.src('./src/js/*.js'),
+      sourcemaps.init(),
+      // jshint(),
+      concat('static.js'),
+      babel({
+        presets: ['es2015']
+      }),
+      // rename({
+      //   suffix: '.min'
+      // }),
+      //压缩操作
+      // uglify({}),
+      // sourcemaps.write('.'),
+      gulp.dest('./dist/js')
+    ],
+    cb //函数有参数
+  );
+});
+
+gulp.task('es6', function() {
+  gulp.src('src/js/*.js')
+    .pipe($.plumber())
+    .pipe($.babel({　　　　
+      presets: ['es2015']　　　　　
+    }))
+    .pipe(gulp.dest('src/es5'));
+});
+
+//图片压缩
+gulp.task('Imagemin', function() {
+  gulp.src('src/img/*.{png,jpg,gif,ico}')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', ['serve']);
